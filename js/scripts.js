0b4a9ba8004ba2502737439a3cd9781c4bcad5e1
Student <example@example.com>
2019-10-24 13:43:56 -0700

    add div containers for each rollercoaster, add descriptions of each div, add submit button, add prompt asking for height, add images

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+  $("form#height").submit(function(event){
+    var height = parseInt(prompt("How tall are you? (in inches)"));
+    
+
+
+    event.preventDefault();
+  });
+
+
+
+
+
+});
